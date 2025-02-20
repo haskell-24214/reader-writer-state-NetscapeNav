4ccfdf4f9ce4fc5cd22b98a1ccafaf7a5345e107
NetscapeNav <netscapenavigator@yahoo.com>
2025-02-20 10:39:59 +0700

    git commit

--- src/Tier2/Writer.hs
+++ src/Tier2/Writer.hs
@@ -2,6 +2,14 @@ module Tier2.Writer (collectAndSumInOrder) where
 
 import Control.Monad.Writer
 import Tier0.Writer (Tree (..))
+import Data.Monoid (Sum (..))
 
 collectAndSumInOrder :: Num a => Tree a -> Writer (Sum a) [a]
-collectAndSumInOrder = undefined
+collectAndSumInOrder (Leaf x) = do
+    tell (Sum x) 
+    return [x]
+collectAndSumInOrder (Branch left x right) = do
+    leftResult <- collectAndSumInOrder left
+    tell (Sum x)
+    rightResult <- collectAndSumInOrder right
+    return (leftResult ++ [x] ++ rightResult)
